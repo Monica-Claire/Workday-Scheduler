2524bd4b86cee6f50f4f4e655bc8e63d824c8205
Monica-Claire <monicaclairewallace@gmail.com>
2022-05-25 07:19:47 -0400

    working script

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,40 @@
 const d = new Date();
 document.getElementById("currentDay").innerHTML = d;
 
-//editable time blocks for tasks
-
-//event listener for click
-document.getElementById("hour9").addEventListener("keypress", console.log("press"));
-//convert to a variable to be able to store it?
-
-//save when click save
-//add event listener to catch button click
-document.getElementById("save").addEventListener("click", console.log("save"));
-
-//save to local storage when press save button
-localStorage.setItem();
-
-//pull from local storage on refresh
-const tasks = localStorage.getItem();
-
 //add color coding based on time
+$(document).ready(function(){
+    function hourUpdater(){
+        let currentHour = moment().hours()
+        $(".hour").each(function(){
+            let hour = $(this).attr("id")
+            if (hour < currentHour) {
+                $(this).addClass("past")
+            } else if (hour === currentHour) {
+                $(this).addClass("present")
+            } else {$(this).addClass("future")}
+        })
+    }
+    hourUpdater()
+    //pull from local storage to show tasks on refresh 
+    //use .each
+    function refresh(){
+        $(".textarea").each(function(){
+            let description = localStorage.getItem($(this).parent().attr("id"))
+            $(this).val(description)
+        })
+    }
+    refresh()
+})
+
+
+
+//save button functionality
+$(".saveBtn").on("click", function(){
+    //saves text as var value
+    let value = $(this).siblings(".textarea").val()
+    //saves time as var time
+    let time = $(this).parent().attr("id")
+    //saves both variables to local storage
+    localStorage.setItem(time, value)
+})
+
